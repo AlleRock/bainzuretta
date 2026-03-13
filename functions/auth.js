c9ab99dcdd23ecd5eeb571d6832bc90f049241a8
AlleRock <m8online@gmail.com>
2026-03-13 22:09:09 +0100

    Delete functions directory

--- functions/auth.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const clientId = context.env.STRAVA_CLIENT_ID;
-  const redirectUri = `https://bainzuretta.pages.dev/functions/callback`;
-  const scope = "activity:read_all";
-  const url = `https://www.strava.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${scope}`;
-  return Response.redirect(url, 302);
-}
